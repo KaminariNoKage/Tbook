8a7d40bf1d7cc1995ade897e8685ae87eed19d2a
kaustin <kaustin@ubuntu.(none)>
2013-03-22 01:38:57 -0400

    First pass

---
+++ app.js
@@ -0,0 +1,54 @@
+
+/**
+ * Module dependencies.
+ */
+
+var express = require('express')
+  , routes = require('./routes')
+  , user = require('./routes/user')
+  , http = require('http')
+  , path = require('path')
+  , mongoose = require('mongoose')
+  , Facebook = require('facebook-node-sdk');
+
+var app = express();
+
+app.configure(function(){
+  app.set('port', process.env.PORT || 3000);
+  app.set('views', __dirname + '/views');
+  app.set('view engine', 'jade');
+  app.use(express.favicon());
+  app.use(express.logger('dev'));
+  app.use(express.bodyParser());
+  app.use(express.methodOverride());
+  app.use(express.cookieParser('your secret here'));
+  app.use(express.session());
+  app.use(Facebook.middleware({ appId: '367123806734507', secret: '7c554b7c5946417761d5cd68faf4be6d' }));
+  app.use(app.router);
+  app.use(express.static(path.join(__dirname, 'public')));
+  mongoose.connect(process.env.MONGOLAB_URI || 'mongodb://localhost/tbook');
+});
+
+app.configure('development', function(){
+  app.use(express.errorHandler());
+});
+
+function facebookGetUser() {
+  return function(req, res, next) {
+    req.facebook.getUser( function(err, user) {
+      if (!user || err){
+        res.redirect('/login');
+      } else {
+        req.user = user;
+        next();
+      }
+    });
+  }
+}
+
+app.get('/', routes.index);
+app.get('/users', user.list);
+
+http.createServer(app).listen(app.get('port'), function(){
+  console.log("Express server listening on port " + app.get('port'));
+});
---
+++ routes/user.js
@@ -0,0 +1,10 @@
+
+/*
+ * GET users listing.
+ */
+
+exports.login = function(req, res){
+	req.facebook.api('/me', function(err, data) {
+		
+	});
+};
